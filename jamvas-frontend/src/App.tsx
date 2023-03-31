d499f19d98bb46fa4d048e45271babfd9f1f9c2a
ynnckth <yannickstreit@hotmail.com>
2023-03-31 18:03:16 +0800

    Initialize frontend using Vite+Typescript template

---
+++ jamvas-frontend/src/App.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      {/* TODO: Insert jamvas logo */}
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Jamvas</h1>
+      <div className="card">
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+};
+
+export default App;
