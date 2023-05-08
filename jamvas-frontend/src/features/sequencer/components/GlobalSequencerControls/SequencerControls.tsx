3ca2a5901168f7b00bae7bb9ca33b63785a13e3b
ynnckth <yannickstreit@hotmail.com>
2023-05-08 17:18:32 +0800

    Replace slider with chakra-ui slider

--- jamvas-frontend/src/features/sequencer/components/GlobalSequencerControls/SequencerControls.tsx
+++ jamvas-frontend/src/features/sequencer/components/GlobalSequencerControls/SequencerControls.tsx
@@ -6,6 +6,7 @@ import { setBpm } from "../../sequencerThunks";
 import { FaPause, FaPlay } from "react-icons/fa";
 import { MAX_BPM, MIN_BPM } from "../../constants";
 import { debounce } from "lodash";
+import { Box, Slider, SliderFilledTrack, SliderThumb, SliderTrack } from "@chakra-ui/react";
 
 interface Props {
   onStartSequence: () => void;
@@ -23,7 +24,7 @@ export const SequencerControls: React.FC<Props> = ({ onStartSequence, onStopSequ
     }
   };
 
-  const onUpdateBpm = (event: any) => dispatch(setBpm({ newBpm: Number(event.target.value) }));
+  const onUpdateBpm = (newBpm: number) => dispatch(setBpm({ newBpm: newBpm }));
 
   const toggleStopped = () => (isSequencerStopped ? onStartSequence() : onStopSequence());
 
@@ -37,15 +38,19 @@ export const SequencerControls: React.FC<Props> = ({ onStartSequence, onStopSequ
       <div className="play-stop-button" onClick={() => toggleStopped()} data-test-id="play-stop-button">
         {isSequencerStopped ? <FaPlay size={20} /> : <FaPause size={20} />}
       </div>
-      <div className="bpm-slider">
-        <input
-          type="range"
+      <Box display="flex" alignItems="center" width="250px">
+        <Slider
+          defaultValue={sequencerConfiguration?.bpm}
           min={MIN_BPM}
           max={MAX_BPM}
-          defaultValue={sequencerConfiguration?.bpm}
           onChange={debounce(onUpdateBpm, 200)}
-        />
-      </div>
+        >
+          <SliderTrack bg="red.100">
+            <SliderFilledTrack bg="tomato" />
+          </SliderTrack>
+          <SliderThumb boxSize={5} />
+        </Slider>
+      </Box>
     </div>
   );
 };
