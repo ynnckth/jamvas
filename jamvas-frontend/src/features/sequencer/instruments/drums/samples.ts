077676164d622a4e97ca2c119bb3376ec68c577b
ynnckth <yannickstreit@hotmail.com>
2023-05-10 18:02:37 +0800

    Fix sample url path when running in "production" to include the /jamvas prefix

--- jamvas-frontend/src/features/sequencer/instruments/drums/samples.ts
+++ jamvas-frontend/src/features/sequencer/instruments/drums/samples.ts
@@ -4,30 +4,41 @@ export type Sample = {
   urlPath: string;
 };
 
+/**
+ * When running in production mode, the base path of the application is /jamvas
+ * Therefore, we need to prepend this url fragment in that case.
+ */
+const prependUrlFragmentIfNotDevelopment = (sampleUrl: string) => {
+  if (!import.meta.env.DEV) {
+    return `/jamvas/${sampleUrl}`;
+  }
+  return sampleUrl;
+};
+
 export const samples: Sample[] = [
   {
     name: "kick",
     key: "C1",
-    urlPath: "/assets/sound_samples/808/BD2525.WAV",
+    urlPath: prependUrlFragmentIfNotDevelopment("/assets/sound_samples/808/BD2525.WAV"),
   },
   {
     name: "snare",
     key: "E1",
-    urlPath: "/assets/sound_samples/808/SD2510.WAV",
+    urlPath: prependUrlFragmentIfNotDevelopment("/assets/sound_samples/808/SD2510.WAV"),
   },
   {
     name: "clap",
     key: "F1",
-    urlPath: "/assets/sound_samples/808/CP.WAV",
+    urlPath: prependUrlFragmentIfNotDevelopment("/assets/sound_samples/808/CP.WAV"),
   },
   {
     name: "hihat_closed",
     key: "G1",
-    urlPath: "/assets/sound_samples/808/CH.WAV",
+    urlPath: prependUrlFragmentIfNotDevelopment("/assets/sound_samples/808/CH.WAV"),
   },
   {
     name: "hihat_open",
     key: "A1",
-    urlPath: "/assets/sound_samples/808/OH25.WAV",
+    urlPath: prependUrlFragmentIfNotDevelopment("/assets/sound_samples/808/OH25.WAV"),
   },
 ];
