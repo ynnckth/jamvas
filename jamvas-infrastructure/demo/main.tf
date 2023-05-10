8f758f48ff7f88191208627e9aa19374461a7ec7
ynnckth <yannickstreit@hotmail.com>
2023-05-10 13:07:41 +0800

    Add app service plan and app service to infrastructure

--- jamvas-infrastructure/demo/main.tf
+++ jamvas-infrastructure/demo/main.tf
@@ -34,33 +34,32 @@ resource "azurerm_resource_group" "rg" {
   location = local.location
 }
 
-# TODO: enable again
-## App Service Plan
-#resource "azurerm_service_plan" "app_service_plan" {
-#  name = "${local.prefix}-app-service-plan"
-#  resource_group_name = azurerm_resource_group.rg.name
-#  location = azurerm_resource_group.rg.location
-#  os_type = "Linux"
-#  sku_name = "F1"
-#}
-#
-## App Service
-#resource "azurerm_linux_web_app" "jamvas-backend" {
-#  name = "${local.prefix}-backend"
-#  resource_group_name = azurerm_resource_group.rg.name
-#  location = azurerm_resource_group.rg.location
-#  service_plan_id = azurerm_service_plan.app_service_plan.id
-#
-#  site_config {
-#    linux_fx_version = "DOCKER|ghcr.io/ynnckth/jamvas:latest"
-#    always_on = "true"
-#  }
-#
-#  app_settings = {
-#    DOCKER_REGISTRY_SERVER_URL = "https://ghcr.io"
-#    DOCKER_REGISTRY_SERVER_USERNAME = var.github_packages.username
-#    DOCKER_REGISTRY_SERVER_PASSWORD = var.github_packages.password
-#    WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
-#    WEBSITE_HTTPLOGGING_RETENTION_DAYS = 5
-#  }
-#}
+# App Service Plan
+resource "azurerm_service_plan" "app_service_plan" {
+  name = "${local.prefix}-app-service-plan"
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+  os_type = "Linux"
+  sku_name = "F1"
+}
+
+# App Service
+resource "azurerm_linux_web_app" "jamvas-backend" {
+  name = "${local.prefix}-backend"
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+  service_plan_id = azurerm_service_plan.app_service_plan.id
+
+  site_config {
+    linux_fx_version = "DOCKER|ghcr.io/ynnckth/jamvas:latest"
+    always_on = "true"
+  }
+
+  app_settings = {
+    DOCKER_REGISTRY_SERVER_URL = "https://ghcr.io"
+    DOCKER_REGISTRY_SERVER_USERNAME = var.github_packages.username
+    DOCKER_REGISTRY_SERVER_PASSWORD = var.github_packages.password
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
+    WEBSITE_HTTPLOGGING_RETENTION_DAYS = 5
+  }
+}
