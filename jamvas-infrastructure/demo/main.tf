18b15b3967e64adb6b6de9706969e7fdf02c8160
ynnckth <yannickstreit@hotmail.com>
2023-05-11 15:02:30 +0800

    Remove app service setting to store logs in file system since logs are already forwarded from console to log analytics workspace with the configured diagnostic setting

--- jamvas-infrastructure/demo/main.tf
+++ jamvas-infrastructure/demo/main.tf
@@ -64,18 +64,6 @@ resource "azurerm_linux_web_app" "jamvas-backend" {
     DOCKER_REGISTRY_SERVER_PASSWORD = var.github_packages_password
     APPINSIGHTS_INSTRUMENTATIONKEY  = azurerm_application_insights.jamvas-app-insights.instrumentation_key
   }
-
-  logs {
-    http_logs {
-      file_system {
-        retention_in_days = 1
-        retention_in_mb   = 100
-      }
-    }
-    application_logs {
-      file_system_level = "Information" # In the F1 tier we have 1GB disk space available
-    }
-  }
 }
 
 # Log analytics workspace
@@ -89,14 +77,15 @@ resource "azurerm_log_analytics_workspace" "jamvas-log-analytics-workspace" {
 }
 
 resource "azurerm_application_insights" "jamvas-app-insights" {
-  name                 = "${local.prefix}-app-insights"
-  location             = azurerm_resource_group.rg.location
-  resource_group_name  = azurerm_resource_group.rg.name
-  application_type     = "other"
-  retention_in_days    = 30
-  daily_data_cap_in_gb = 0.1 # ~100MB
-  workspace_id         = azurerm_log_analytics_workspace.jamvas-log-analytics-workspace.id
+  name                       = "${local.prefix}-app-insights"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  application_type           = "other"
+  retention_in_days          = 30
+  daily_data_cap_in_gb       = 0.1 # ~100MB
+  workspace_id               = azurerm_log_analytics_workspace.jamvas-log-analytics-workspace.id
   internet_ingestion_enabled = false
+  internet_query_enabled     = false
 }
 
 # App service's diagnostic setting to ship application logs from file system to log analytics workspace
@@ -108,7 +97,7 @@ resource "azurerm_monitor_diagnostic_setting" "jamvas-application-logs-setting" 
     category = "AppServiceConsoleLogs"
     retention_policy {
       enabled = true
-      days = 1
+      days    = 1
     }
   }
 
