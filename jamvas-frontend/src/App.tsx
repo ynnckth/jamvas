49f64e888f8e66c74810ebd5c6656864d055ed8e
ynnckth <yannickstreit@hotmail.com>
2023-05-12 16:10:50 +0800

    Include branch name in application version information in frontend

--- jamvas-frontend/src/App.tsx
+++ jamvas-frontend/src/App.tsx
@@ -14,6 +14,7 @@ const App: React.FC = () => {
     console.group("App version");
     console.log("Build version: ", import.meta.env.VITE_BUILD_VERSION);
     console.log("Commit hash: ", import.meta.env.VITE_COMMIT_HASH);
+    console.log("Branch name: ", import.meta.env.VITE_BRANCH_NAME);
     console.groupEnd();
   }, []);
 
